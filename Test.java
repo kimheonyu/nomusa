1203e2508eb1a1de21cf5a72f095d708c811ad85
heonyukim <heonyu.kim@nrise.net>
2012-08-22 17:38:27 +0900

    test gggg

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ class Test
 {
 	public static void main(String[] args) throws Exception
 	{
-		tttttttttytyytytyttytytttyytt
+		tttttttttytyytytyttytytttyytt111111
 		111111213132132131231232123131232	
 	}
 }
